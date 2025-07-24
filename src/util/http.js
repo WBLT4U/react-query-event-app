a6a238127434d28f6dec9109bec7eef8335f5453
Taofiq Abdulsalam <taofiqabdulsalam48@gmail.com>
2025-07-24 16:09:48 +0100

    render api link

--- src/util/http.js
+++ src/util/http.js
@@ -3,7 +3,7 @@ import { QueryClient } from '@tanstack/react-query';
 export const queryClient = new QueryClient();
 
 export async function fetchEvents({ signal, searchTerm, max }) {
-  let url = 'http://localhost:3000/events';
+  let url = 'https://react-query-event-app.onrender.com/events';
 
   if (searchTerm && max) {
     url += '?search=' + searchTerm + '&max=' + max;
@@ -29,7 +29,7 @@ export async function fetchEvents({ signal, searchTerm, max }) {
 
 
 export async function createNewEvent(eventData) {
-  const response = await fetch(`http://localhost:3000/events`, {
+  const response = await fetch(`https://react-query-event-app.onrender.com/events`, {
     method: 'POST',
     body: JSON.stringify(eventData),
     headers: {
@@ -50,7 +50,7 @@ export async function createNewEvent(eventData) {
 }
 
 export async function fetchSelectableImages({ signal }) {
-  const response = await fetch(`http://localhost:3000/events/images`, { signal });
+  const response = await fetch(`https://react-query-event-app.onrender.com/events/images`, { signal });
 
   if (!response.ok) {
     const error = new Error('An error occurred while fetching the images');
@@ -65,7 +65,7 @@ export async function fetchSelectableImages({ signal }) {
 }
 
 export async function fetchEvent({ id, signal }) {
-  const response = await fetch(`http://localhost:3000/events/${id}`, { signal });
+  const response = await fetch(`https://react-query-event-app.onrender.com/events/${id}`, { signal });
 
   if (!response.ok) {
     const error = new Error('An error occurred while fetching the event');
@@ -81,7 +81,7 @@ export async function fetchEvent({ id, signal }) {
 
 
 export async function deleteEvent({ id }) {
-  const response = await fetch(`http://localhost:3000/events/${id}`, {
+  const response = await fetch(`https://react-query-event-app.onrender.com/events/${id}`, {
     method: 'DELETE',
   });
 
@@ -96,7 +96,7 @@ export async function deleteEvent({ id }) {
 }
 
 export async function updateEvent({ id, event }) {
-  const response = await fetch(`http://localhost:3000/events/${id}`, {
+  const response = await fetch(`https://react-query-event-app.onrender.com/events/${id}`, {
     method: 'PUT',
     body: JSON.stringify({ event }),
     headers: {
